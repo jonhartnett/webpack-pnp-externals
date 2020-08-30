d2a2042cb13f8f931dcdb39e4dec66675f12675d
jonhartnett <jonehartnett@yahoo.com>
2020-08-30 18:01:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+## Webpack PnP Externals
+Allows you to exclude modules installed using Yarn's [PnP](https://classic.yarnpkg.com/en/docs/pnp/) from the bundle.
+Mostly useful for the backend.
+Intended to be used along side [webpack-node-externals](https://github.com/liady/webpack-node-externals).
+
+### Installation
+```shell script
+npm install -D webpack-pnp-externals
+```
+
+### Usage
+Webpack Documentation: [Externals](https://webpack.js.org/configuration/externals/).
+```js
+const {WebpackPnpExternals} = require('webpack-pnp-externals');
+
+module.exports = {
+    externals: [
+        WebpackPnpExternals()
+    ]
+};
+```
+This will exclude all PnP modules from the bundle and instead load them with `require`.
+
+### Options
+* `include` Only externalizes the matching requests.
+    Can be one of the following types:
+    * `string` A string to match the request against exactly.
+    * `RegExp` A regex pattern to match the request against.
+    * `function: (request: string, resolution: string) -> boolean`
+        A custom function to check whether to externalize the request.
+        * `request` The content of the import statement.
+        * `resolution` The file which the request resolved to. 
+    * `Array` An array of any of the preceding to be or-ed together.
+* `exclude` Same as `include`, but excludes requests that match.
+* `importType` (default `'commonjs'`) 
+    The [external type](https://webpack.js.org/configuration/externals/#externalstype) to use when loading the externalized modules.
+    Can be one of the following types:
+    * `string` A type to use for all modules.
+    * `function: (request: string, resolution: string) -> string`
+        A custom function to get the external type for a given request and resolution.
+
+### License
+[MIT](./LICENSE)
---
+++ src/index.js
@@ -0,0 +1,56 @@
+import Path from 'path';
+
+export function WebpackPnpExternals(options={}) {
+    let {include, exclude, importType='commonjs'} = options;
+    let pnpApi;
+    try{
+        pnpApi = require('pnpapi');
+    }catch(err){
+        if(err.code !== 'MODULE_NOT_FOUND')
+            throw err;
+        pnpApi = null;
+    }
+    return (context, request, callback) => {
+        if(pnpApi == null)
+            return callback();
+        //don't check relative or absolute requires
+        if(/^\.\.?[/\\]/.test(request) || Path.isAbsolute(request))
+            return callback();
+        let resolution;
+        try{
+            resolution = pnpApi.resolveRequest(request, context, {
+                considerBuiltins: false
+            });
+        }catch(err){
+            if(err.code === 'MODULE_NOT_FOUND')
+                return callback();
+            throw err;
+        }
+        if(include != null && !isMatch(include, request, resolution))
+            return callback();
+        if(exclude != null && isMatch(exclude, request, resolution))
+            return callback();
+        let type = importType;
+        if(type instanceof Function)
+            type = type(request, resolution);
+        return callback(null, request, type);
+    };
+}
+
+function isMatch(pattern, request, ...args){
+    if(typeof pattern === 'string'){
+        return pattern === request;
+    }else if(pattern instanceof RegExp){
+        return pattern.test(request);
+    }else if(pattern instanceof Function){
+        return pattern(request, ...args);
+    }else if(pattern instanceof Array){
+        for(let subPattern of pattern){
+            if(isMatch(subPattern, request, ...args))
+                return true;
+        }
+        return false;
+    }else{
+        throw new Error(`Unexpected pattern type: ${pattern}`);
+    }
+}
